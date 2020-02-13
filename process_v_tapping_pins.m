82356df84d5940b23bd6d2e97c981a6582485425
Tebazil12 <lizzie.stone@hotmail.com>
2020-02-13 19:17:01 +0000

    Turn TRACKING ON (very important) and recollect data

--- process_v_tapping_pins.m
+++ process_v_tapping_pins.m
@@ -1,15 +1,25 @@
-load('H:\git\tactile-core\matlab\demos\voronoi_data\2020-02-13_1816_tapping\timestep21_tapping_edge')
+load('H:\git\tactile-core\matlab\demos\voronoi_data\2020-02-13_1847_tapping\timestep21_tapping_edge')
 clf
 % there are 6 poses, so compare repeatability in each pose
 
 % for i = 1:6
 % %     figure(i)
 %     subplot(2,3,i);
+all_x = [];
+all_y =[];
     for movement = 1:21
         if movement>21
             break;
         end
-        scatter(all_pins{movement}(:,:,1), all_pins{movement}(:,:,2))
+%         scatter(all_pins{movement}(:,:,1), all_pins{movement}(:,:,2))
+        all_x = [all_x; all_pins{movement}(:,:,1)];
+        all_y = [all_y; all_pins{movement}(:,:,2)];
+        hold on
+    end
+%     plot(all_x,all_y);
+    
+    for pin_num = 1:37
+        plot(all_x(:,pin_num),all_y(:,pin_num),"+-");
         hold on
     end
     hold off
