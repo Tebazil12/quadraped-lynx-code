e97af5f630e153235c835d0e082983f81b3df659
Tebazil12 <lizzie.stone@hotmail.com>
2021-07-20 15:26:28 +0100

    Fix quad comms bug
    Function was not sending encoded string but example message

--- python/robot_legmoves_testing.py
+++ python/robot_legmoves_testing.py
@@ -8,13 +8,13 @@ def serial_write_read(ser, msg):
     # need to match format that matlab used
     msg = msg + "\n"
     encoded_msg = msg.encode("utf-8")
-    ser.write(b'start_pose\n')     # write a string
+    ser.write(encoded_msg)     # write a string
     response = ser.readline().decode("utf-8") # decode removes b' \r\n' from message
 
     return response
 
 # NB, robot batteries must be switched on, even if not trying to move legs!
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+ser = serial.Serial('/dev/ttyUSB0', baudrate=9600)  # open serial port
 time.sleep(1.5) # in matlab this is needed to allow serial port time to open
 
 print(ser.name)         # check which port was really used
